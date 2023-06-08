353927a00749dc9925e17bda4bd6525101f4b400
RaviVendraminideAlmeida <ravi.almeida@etec.sp.gov.br>
2023-06-08 01:23:30 -0300

    Added file explaining how to work with variables and comments

---
+++ shell-files/variables-and-comments.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+# Global Variables
+
+# Bash's binary location
+echo $BASH
+#Bash Version
+echo $BASH_VERSION
+#Home Directory
+echo $HOME
+#Where this script is located
+echo $PWD
+
+# User variables
+
+# Declaration names should not start w/ numbers
+name=Ravi
+value=42069
+# Usage with $ Sign
+echo $name
+echo $value
